e0b21ff874b4f05c30616c68107e1e375f828a04
ANDRE <klai1@mail.ru>
2025-02-20 01:08:51 +0400

    Version

--- src/main/java/com/astondz/astontz/conroller/HallowWorld.java
+++ src/main/java/com/astondz/astontz/conroller/HallowWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HallowWorld {
 
-    @GetMapping("/www")
+    @GetMapping
     String hallowWorld() {
         return "Hallow World \n Version2" ;
     }
